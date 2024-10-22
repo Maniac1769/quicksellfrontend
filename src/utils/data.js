ccb9872f2541d9f2e4a678d4087974934fd35bbb
Abhinav Mathur <abhmat1990@gmail.com>
2024-10-22 19:54:07 +0530

    Initial commit

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,35 +1,39 @@
-import { BsThreeDots, BsFillInfoSquareFill } from "react-icons/bs"
-import { FaCheckCircle } from "react-icons/fa";
-import { FaCircleXmark, FaRegCircle } from "react-icons/fa6";
-import { TbCircleDotted } from "react-icons/tb";
+import ThreeDotsIcon from '../icons_FEtask/3 dot menu.svg';
+import InfoSquareIcon from '../icons_FEtask/SVG - Urgent Priority colour.svg';
+import Low from '../icons_FEtask/Img - Low Priority.svg';
+import Medium from '../icons_FEtask/Img - Medium Priority.svg';
+import High from '../icons_FEtask/Img - High Priority.svg';
+import CircleDottedIcon from '../icons_FEtask/Backlog.svg';
+import RegCircleIcon from '../icons_FEtask/To-do.svg';
+import CheckCircleIcon from '../icons_FEtask/Done.svg';
+import CircleXmarkIcon from '../icons_FEtask/Cancelled.svg';
 import { RxHalf2 } from "react-icons/rx";
-import { GiNetworkBars } from "react-icons/gi";
-
 const bgColors = ["#B57136", "#868728", "#4D9446", "#5F80E4"];
 
 export const priorities = [
-    { title: "no priority", color: "gray", icon: <BsThreeDots /> },
-    { title: "low", color: "lightgray", icon: <GiNetworkBars /> },
-    { title: "medium", color: "gray", icon: <GiNetworkBars /> },
-    { title: "high", color: "black", icon: <GiNetworkBars /> },
-    { title: "urgent", color: "orange", icon: <BsFillInfoSquareFill /> }
+    { title: "no priority", color: "gray", icon: <img src={ThreeDotsIcon} alt="No Priority" /> },
+    { title: "urgent", color: "orange", icon: <img src={InfoSquareIcon} alt="Urgent" /> },
+    { title: "high", color: "black", icon: <img src={High} alt="High Priority" /> },
+    { title: "medium", color: "gray", icon: <img src={Medium} alt="Medium Priority" /> },
+    { title: "low", color: "lightgray", icon: <img src={Low} alt="Low Priority" /> }
 ];
 export const status = [
-    { title: "backlog", color: "black", icon: <TbCircleDotted /> },
-    { title: "todo", color: "lightgrey", icon: < FaRegCircle /> },
-    { title: "in progress", color: "#EBCB62", icon: <RxHalf2 /> },
-    { title: "done", color: "#606ACB", icon: <FaCheckCircle /> },
-    { title: "cancelled", color: "gray", icon: <FaCircleXmark /> },
+    { title: "backlog", color: "black", icon: <img src={CircleDottedIcon} alt="Backlog" /> },
+    { title: "todo", color: "lightgrey", icon: <img src={RegCircleIcon} alt="Todo" /> },
+    { title: "in progress", color: "#EBCB62", icon: <RxHalf2 />},
+    { title: "done", color: "#606ACB", icon: <img src={CheckCircleIcon} alt="Done" /> },
+    { title: "cancelled", color: "gray", icon: <img src={CircleXmarkIcon} alt="Cancelled" /> },
 ];
+
 
 export const statusIcons = {
     backlog: {
         color: "black",
-        icon: <TbCircleDotted />,
+        icon: <img src={CircleDottedIcon} alt="Backlog" />,
     },
     todo: {
         color: "lightgrey",
-        icon: <FaRegCircle />,
+        icon: <img src={RegCircleIcon} alt="Todo" />,
     },
     "in progress": {
         color: "#EBCB62",
@@ -37,13 +41,14 @@ export const statusIcons = {
     },
     done: {
         color: "#606ACB",
-        icon: <FaCheckCircle />,
+        icon: <img src={CheckCircleIcon} alt="Done" />,
     },
     cancelled: {
         color: "gray",
-        icon: <FaCircleXmark />,
+        icon: <img src={CircleXmarkIcon} alt="Cancelled" />,
     },
 };
+
 export const generateIntials = (name) => {
     return name.split(' ').map(word => word.charAt(0)).join('');
 }
